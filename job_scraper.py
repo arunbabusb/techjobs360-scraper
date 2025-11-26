86c8e6343f117293620924e89b56b07eb5f01d34
arunbabusb <arunbabusb@gmail.com>
2025-11-26 19:04:45 +0530

    Fix indentation for arbeitnow, jobicy, himalayas, linkedin handlers

--- job_scraper.py
+++ job_scraper.py
@@ -635,15 +635,15 @@ def main():
                         elif stype == "indeed":
                             if src.get("enabled_html", False):
                                 candidate_jobs += parse_indeed(query or qtext, city, limit=src.get("limit", 20))
-                elif stype == "arbeitnow":
-                    candidate_jobs += query_arbeitnow(qtext, limit=src.get("limit", 50))
-                elif stype == "jobicy":
-                    candidate_jobs += query_jobicy(qtext, limit=src.get("limit", 50))
-                elif stype == "himalayas":
-                    candidate_jobs += query_himalayas(qtext, limit=src.get("limit", 40))
-                elif stype == "linkedin":
-                    if src.get("enabled_html", False):
-                        candidate_jobs += parse_linkedin(query or qtext, city, limit=src.get("limit", 15))                        elif stype == "html":
+                    elif stype == "arbeitnow":
+                        candidate_jobs += query_arbeitnow(qtext, limit=src.get("limit", 50))
+                    elif stype == "jobicy":
+                        candidate_jobs += query_jobicy(qtext, limit=src.get("limit", 50))
+                    elif stype == "himalayas":
+                        candidate_jobs += query_himalayas(qtext, limit=src.get("limit", 40))
+                    elif stype == "linkedin":
+                        if src.get("enabled_html", False):
+                            candidate_jobs += parse_linkedin(query or qtext, city, limit=src.get("limit", 15))                        elif stype == "html":
                             endpoint = src.get("endpoint")
                             if endpoint:
                                 try:
