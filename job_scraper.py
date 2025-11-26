17d6f2b9466bb43b783be06a0240e6577621ca82
arunbabusb <arunbabusb@gmail.com>
2025-11-26 18:54:10 +0530

    Fix source handler syntax errors and add proper handlers for arbeitnow, jobicy, himalayas

--- job_scraper.py
+++ job_scraper.py
@@ -631,20 +631,19 @@ def main():
                         elif stype == "remoteok":
                             candidate_jobs += query_remoteok(qtext, limit=src.get("limit", 80))
                         elif stype == "weworkremotely":
-                            candidate_jobs += elif stype == "linkedin"(qtext, limit=src.get("limit", 30))
+                            candidate_jobs += query_weworkremotely(qtext, limit=src.get("limit", 40))
                         elif stype == "indeed":
                             if src.get("enabled_html", False):
                                 candidate_jobs += parse_indeed(query or qtext, city, limit=src.get("limit", 20))
-                        elif stype == "linkedin":
-                            if src.get("enabled_html", False):
-                                                elif stype == "arbeitnow":
+                elif stype == "arbeitnow":
                     candidate_jobs += query_arbeitnow(qtext, limit=src.get("limit", 50))
                 elif stype == "jobicy":
                     candidate_jobs += query_jobicy(qtext, limit=src.get("limit", 50))
                 elif stype == "himalayas":
                     candidate_jobs += query_himalayas(qtext, limit=src.get("limit", 40))
-                                candidate_jobs += parse_linkedin(query or qtext, city, limit=src.get("limit", 15))
-                        elif stype == "html":
+                elif stype == "linkedin":
+                    if src.get("enabled_html", False):
+                        candidate_jobs += parse_linkedin(query or qtext, city, limit=src.get("limit", 15))                        elif stype == "html":
                             endpoint = src.get("endpoint")
                             if endpoint:
                                 try:
